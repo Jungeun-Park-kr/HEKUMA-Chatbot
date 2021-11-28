8082aa9dd1b3a1ff5426ec112be7f93688d192f3
lenzm <lenzm@hs-augsburg.de>
2021-11-28 17:20:38 +0100

    reformatted yaml files; defined basic consturct (actions; nlu; rules; domain) for use case 1; still to do is the actionscode

--- code/Hekuma_Rasa_Project/actions/actions.py
+++ code/Hekuma_Rasa_Project/actions/actions.py
@@ -5,7 +5,6 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -23,13 +22,13 @@ class ActionMyFirstBoolean(Action):
         return "action_utter_supply_myfirstboolean_info"
 
     async def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]: #TODO: check if domain necessary
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:  # TODO: check if domain necessary
 
-        url = "opc.tcp://10.0.0.107:4840" 
+        url = "opc.tcp://10.0.0.107:4840"
         async with Client(url=url) as client:
             var = client.get_node("ns=1;s=AGENT.OBJECTS.MyFirstBoolean")
-            dispatcher.utter_message(text=f"Value of my MyFirstBoolean is {await var.read_value()}") 
+            dispatcher.utter_message(text=f"Value of my MyFirstBoolean is {await var.read_value()}")
 
         return []
 
@@ -40,17 +39,18 @@ class ActionSafetyDoor(Action):
         return "action_utter_supply_safety_door_info"
 
     async def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]: #TODO: check if domain necessary
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:  # TODO: check if domain necessary
 
-        door_number = next(tracker.get_latest_entity_values("door_number"), None)
+        door_number = next(
+            tracker.get_latest_entity_values("door_number"), None)
 
-        url = "opc.tcp://10.0.0.107:4840" 
+        url = "opc.tcp://10.0.0.107:4840"
         async with Client(url=url) as client:
-            
+
             s = "AGENT.OBJECTS.Machine.SafetyZones.SafetyZone1.SafetyDoor_" + door_number + ".isOpen"
             node_id = "ns=1;s=" + s
-            #TODO: what if there is safety door with number 'door_number'
+            # TODO: what if there is safety door with number 'door_number'
             door_status_async = client.get_node(node_id)
             door_status = await door_status_async.read_value()
 
@@ -58,10 +58,87 @@ class ActionSafetyDoor(Action):
             # dispatcher.utter_message(text=f"User input safety door number: {door_number}")
             # dispatcher.utter_message(text=f"Door state of number {door_number}: {DoorStates[int(door_number)]}")
 
-            if door_status: # True:open, False:closed
-                dispatcher.utter_message(text=f"Safety door {door_number} is currently unlocked.") 
-            else : 
-                dispatcher.utter_message(text=f"Safety door {door_number} is currently locked.")
+            if door_status:  # True:open, False:closed
+                dispatcher.utter_message(
+                    text=f"Safety door {door_number} is currently unlocked.")
+            else:
+                dispatcher.utter_message(
+                    text=f"Safety door {door_number} is currently locked.")
 
         return []
 
+
+class ActionComponentLastChanged(Action):
+
+    # When was the coil roll changed last time?
+    # The coil roll was last changed today at 2:35 pm, i.e. 2hrs and 2min ago
+
+    def name(self) -> Text:
+        return "action_utter_supply_component_last_changed_info"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        door_number = next(
+            tracker.get_latest_entity_values("door_number"), None)
+
+        url = "opc.tcp://10.0.0.107:4840"
+        async with Client(url=url) as client:
+
+            s = "AGENT.OBJECTS.Machine.SafetyZones.SafetyZone1.SafetyDoor_" + door_number + ".isOpen"
+            node_id = "ns=1;s=" + s
+            # TODO: what if there is safety door with number 'door_number'
+            door_status_async = client.get_node(node_id)
+            door_status = await door_status_async.read_value()
+
+            # # Check user input value and safety door value
+            # dispatcher.utter_message(text=f"User input safety door number: {door_number}")
+            # dispatcher.utter_message(text=f"Door state of number {door_number}: {DoorStates[int(door_number)]}")
+
+            if door_status:  # True:open, False:closed
+                dispatcher.utter_message(
+                    text=f"Safety door {door_number} is currently unlocked.")
+            else:
+                dispatcher.utter_message(
+                    text=f"Safety door {door_number} is currently locked.")
+
+        return []
+
+
+class ActionGetComponentLocation(Action):
+
+    # When was the coil roll changed last time?
+    # The coil roll was last changed today at 2:35 pm, i.e. 2hrs and 2min ago
+
+    def name(self) -> Text:
+        return "action_utter_supply_component_location_info"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        door_number = next(
+            tracker.get_latest_entity_values("door_number"), None)
+
+        url = "opc.tcp://10.0.0.107:4840"
+        async with Client(url=url) as client:
+
+            s = "AGENT.OBJECTS.Machine.SafetyZones.SafetyZone1.SafetyDoor_" + door_number + ".isOpen"
+            node_id = "ns=1;s=" + s
+            # TODO: what if there is safety door with number 'door_number'
+            door_status_async = client.get_node(node_id)
+            door_status = await door_status_async.read_value()
+
+            # # Check user input value and safety door value
+            # dispatcher.utter_message(text=f"User input safety door number: {door_number}")
+            # dispatcher.utter_message(text=f"Door state of number {door_number}: {DoorStates[int(door_number)]}")
+
+            if door_status:  # True:open, False:closed
+                dispatcher.utter_message(
+                    text=f"Safety door {door_number} is currently unlocked.")
+            else:
+                dispatcher.utter_message(
+                    text=f"Safety door {door_number} is currently locked.")
+
+        return []
