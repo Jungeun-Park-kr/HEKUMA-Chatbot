dd78506e658f499c1a349256f111da6933cf2e13
Saliu Bah <saliu.bah@hs-augsburg.de>
2021-12-03 10:09:42 +0100

    test_version_for_rule_triggers

--- code/Hekuma_Rasa_Project/actions/actions.py
+++ code/Hekuma_Rasa_Project/actions/actions.py
@@ -96,3 +96,27 @@ class ActionAlarm(Action):
                 dispatcher.utter_message(text=f"Stop?")
 
         return []
+
+class ActionTellID(Action):
+    """Informs the user about the conversation ID."""
+
+    def name(self) -> Text:
+        return "action_tell_id"
+
+    async def run(
+        self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+
+        conversation_id = tracker.sender_id
+
+        dispatcher.utter_message(f"The ID of this conversation is '{conversation_id}'.")
+        dispatcher.utter_message(
+            f"Trigger an intent with: \n"
+            f'curl -H "Content-Type: application/json" '
+            f'-X POST -d \'{{"name": "EXTERNAL_dry_plant", '
+            f'"entities": {{"plant": "Orchid"}}}}\' '
+            f'"http://localhost:5005/conversations/{conversation_id}'
+            f'/trigger_intent?output_channel=latest"'
+        )
+
+        return []
